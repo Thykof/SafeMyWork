531cd268905084cb2afb43ee4849681b08290599
Thykof <nathan.seva@outlook.fr>
2016-02-28 11:24:43 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='SafeMyWork',
+    version='0.0.1',
+    description='Little app to save your work.',
+    long_description=readme,
+    author='Thykof',
+    author_email='nathan.seva@outlook.fr',
+    url='https://github.com/Thykof/SafeMyWork',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
