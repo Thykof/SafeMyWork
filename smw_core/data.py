5d6d2f964323a8f8b6d678937525e9731b7ff0aa
Thykof <nathan.seva@outlook.fr>
2016-03-12 11:40:33 +0100

    Bump version 0.1

--- smw_core/data.py
+++ smw_core/data.py
@@ -5,7 +5,7 @@
 FILE_CONFIG = 'SMW_conf_file.ini'
 INT_VALUES = ['tiemdelta']
 DEFAULT_CONFIG = {
-    'time_delta': 3,
+    'time_delta': 600,
     'archive_dir': 'safe_docs',
     'delicate_dirs': [],
     'exclude_dirs': [],
