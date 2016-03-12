1d58494bf9aa63f6a5f819cb64078aaa294a48a5
Thykof <nathan.seva@outlook.fr>
2016-03-12 12:32:47 +0100

    Improve atexit

--- SafeMyWork.py
+++ SafeMyWork.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import atexit
+from os import system
 
 from smw_core import mod
 from smw_core import conf
 from smw_core.watcher import Watcher
 
+def quit(config):
+    conf.save_config(config)
+    system('pause')
+
 if __name__ == '__main__':
     config = conf.get_config()
-    watcher = Watcher(config)
-    atexit.register(conf.save_config, config)
+    watcher = Watcher(quit)
+    atexit.register()
     watcher.start()
