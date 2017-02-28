0cd9cf2a04d00b838be788da04e6bc6effde2eb9
Thykof <nathan.seva@outlook.fr>
2017-02-28 19:21:39 +0100

    Improve setting dialog

--- interface/dialog.py
+++ interface/dialog.py
@@ -14,7 +14,6 @@ def del_dir_dialog(parent, directory):
 	return response == Gtk.ResponseType.YES, dialog
 
 class Settings_dial(Gtk.Dialog):
-	"""docstring for Settings"""
 	def __init__(self, parent):
 		Gtk.Dialog.__init__(self, 'Préférences', parent, 0)
 		self.parent = parent
@@ -25,39 +24,19 @@ class Settings_dial(Gtk.Dialog):
 		box = self.get_content_area()
 		box.set_spacing(6)
 
-		box_time = self.create_box_time()
-		box_ext = self.create_box_ext()
-		box_files = self.create_box_files()
-		box_dirs = self.create_box_dirs()
-		box_archive_dir = self.create_box_archive_dir()
 		button_close = Gtk.Button.new_with_label('Femrer')
 		button_close.connect('clicked', self.close)
 
-		# Pack boxes
-		box.pack_start(box_time, False, False, 0)
-		box.pack_start(box_ext, False, False, 0)
-		box.pack_start(box_files, False, False, 0)
-		box.pack_start(box_dirs, False, False, 0)
-		box.pack_start(box_archive_dir, False, False, 0)
-		box.pack_start(button_close, False, False, 0)
-		self.show_all()
-
-	def create_box_time(self):
 		# Set timedelta:
 		box_time = Gtk.Box(spacing=6)
 		adjustment = Gtk.Adjustment(10, 1, 60, 10, 10, 0)
 		self.spinbutton = Gtk.SpinButton(adjustment=adjustment)
 		self.spinbutton.set_digits(0)
 		self.spinbutton.set_value(self.parent.time_delta)
-		timedelta_button = Gtk.Button.new_with_label('Changer')
-		timedelta_button.connect('clicked', self.change_timedelta)
 		box_time.pack_start(Gtk.Label('Scan tous les :'), False, False, 0)
 		box_time.pack_start(self.spinbutton, False, False, 0)
 		box_time.pack_start(Gtk.Label('Minutes. '), False, False, 0)
-		box_time.pack_start(timedelta_button, False, False, 0)
-		return box_time
 
-	def create_box_ext(self):
 		# Exclude extenstions:
 		box_ext = Gtk.Box(spacing=6)
 		box_ext.pack_start(Gtk.Label('Extensions : '), False, False, 0)
@@ -71,9 +50,7 @@ class Settings_dial(Gtk.Dialog):
 		box_ext.pack_start(button_del_ext, False, False, 0)
 		for ext in self.parent.safer.config['extention']:
 			ext_list.append_text(ext)
-		return box_ext
 
-	def create_box_files(self):
 		# Exclude files:
 		box_files = Gtk.Box(spacing=6)
 		box_files.pack_start(Gtk.Label('Fichiers : '), False, False, 0)
@@ -87,9 +64,7 @@ class Settings_dial(Gtk.Dialog):
 		box_files.pack_start(button_del_file, False, False, 0)
 		for file in self.parent.safer.config['filename']:
 			file_list.append_text(file)
-		return box_files
 
-	def create_box_dirs(self):
 		# Exclude directories:
 		box_dirs = Gtk.Box(spacing=6)
 		box_dirs.pack_start(Gtk.Label('Dossiers : '), False, False, 0)
@@ -103,22 +78,32 @@ class Settings_dial(Gtk.Dialog):
 		box_dirs.pack_start(button_del_dirs, False, False, 0)
 		for dirs in self.parent.safer.config['dirname']:
 			dirs_list.append_text(dirs)
-		return box_dirs
 
-	def create_box_archive_dir(self):
 		# Archive directory:
 		box_archive_dir = Gtk.Box(spacing=6)
 		box_archive_dir.pack_start(Gtk.Label('Dossier archive : '), False, False, 0)
 		self.archive_entry = Gtk.Entry()
 		self.archive_entry.set_text(self.parent.safer.destination)
-		archive_button = Gtk.Button.new_with_label('Changer')
-		archive_button.connect('clicked', self.change_archive_dir)
 		box_archive_dir.pack_start(self.archive_entry, False, False, 0)
-		box_archive_dir.pack_start(archive_button, False, False, 0)
-		return box_archive_dir
 
+		# Pack boxes
+		box.pack_start(box_time, False, False, 0)
+		box.pack_start(box_ext, False, False, 0)
+		box.pack_start(box_files, False, False, 0)
+		box.pack_start(box_dirs, False, False, 0)
+		box.pack_start(box_archive_dir, False, False, 0)
+		box.pack_start(button_close, False, False, 0)
+		self.show_all()
 
 	def close(self, *args):
+		# Set new timedelta
+		timedelta = int(self.spinbutton.get_value())
+		self.parent.time_delta = timedelta
+
+		# Set new safe path
+		new = self.archive_entry.get_text()
+		if new != '':
+			self.parent.safer.set_destination(new)
 		self.destroy()
 
 	def add(self, elt, combo_list):
@@ -136,12 +121,3 @@ class Settings_dial(Gtk.Dialog):
 			new = model[tree_iter][0]
 			combo_list.remove(int(combo_list.get_active()))
 			self.parent.safer.config[elt].remove(new)
-
-	def change_timedelta(self, button):
-		timedelta = int(self.spinbutton.get_value())
-		self.parent.time_delta = timedelta
-
-	def change_archive_dir(self, button):
-		new = self.archive_entry.get_text()
-		if new != '':
-			self.parent.safer.set_destination(new)
