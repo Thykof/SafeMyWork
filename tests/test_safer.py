1ad1063be72f9dfe3eec54e3380ceec7d9fb6054
Thykof <nathan.seva@outlook.fr>
2018-03-06 10:59:55 +0100

    Update test

--- tests/test_safer.py
+++ tests/test_safer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from safer import helpers, scan, sync
+from safer import helpers, sync
 
 class BaseTest(object):
 	def setup_method(self, test_method):
@@ -10,9 +10,6 @@ class TestSafer(BaseTest):
     def _test_safer(self):
         path1 = '/home/thykof/Téléchargements'
         path2 = '/home/thykof/Vidéos'
-        myscan = scan.Scan()
-        files1 = myscan.scan_dir(path1)
-        files2 = myscan.scan_dir(path2)
-        mysync = sync.Sync(path1, path2)
-        errors = mysync.run(files1, files2)
+		mysync = sync.Sync(path1, path2)
+		errors = mysync.exec_sync()
         assert errors == (list(), list())
