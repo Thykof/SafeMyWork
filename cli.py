1d130a80e2e4b3aa7682342ac9c26c9f7951a64e
Thykof <thykof@protonmail.ch>
2020-09-13 19:39:52 +0200

    Add cli usage

---
+++ cli.py
@@ -0,0 +1,4 @@
+from cli.copy import copy
+
+if __name__ == '__main__':
+    copy()
---
+++ cli/main.py
@@ -0,0 +1,53 @@
+import os
+import threading
+import asyncio
+
+
+import click
+
+
+from safer.safe import Safer
+
+
+nb_copies_done = 0
+
+@click.command()
+@click.option('-d', '--delta', default=10, help='Number of minutes between copies.')
+@click.option('-s', '--safe_dir', default=None, help='Destination folder.')
+@click.option('-w', '--delicate_dirs', required=True, help='Folder to save.')
+@click.option('-n', '--count', default=0, help='Number of iterations.')
+@click.option('-t', '--type', help='Number of iterations.')
+def copy(delta, safe_dir, delicate_dirs, count, type):
+    config = {
+        'safe_dir': safe_dir,
+        'delicate_dirs': [delicate_dirs],
+        'advanced': False, # disable MAX_DIR_SIZE limit
+        # Exclusion rules:
+        'dirname': [".git"],
+        'dirpath': [],
+        'filename': [],
+        'extention': []
+    }
+    print(config, count)
+
+    loop = asyncio.get_event_loop()
+
+    safer = Safer(config=config)
+
+    if type is None:
+        func = lambda: safer.save_with_filters(loop)
+    elif type == 'copy':
+        func = safer.copy_files
+    elif type == 'update':
+        func = lambda: safer.update(loop)
+
+    def perpetual_scan():
+        global nb_copies_done
+        func()
+        nb_copies_done += 1
+        if nb_copies_done < count:
+            timer = threading.Timer(delta, perpetual_scan)
+            timer.start()
+
+    # delta *= 60
+    perpetual_scan()
